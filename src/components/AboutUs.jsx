dfbe1a8d5f6f7f518c375135bc39358229b4e006
Quan-perfect <anhquan.dao411@gmail.com>
2026-07-29 23:02:03 +0700

    Add AboutUs component with description

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h2>About Us</h2>
+      <p className="about-us-description">
+        Welcome to Paradise Nursery, your ultimate destination for bringing nature indoors. 
+        We are dedicated to providing the healthiest, most vibrant houseplants to purify your air 
+        and brighten your living spaces. Our mission is to connect people with nature and make 
+        plant parenting easy and enjoyable for everyone.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
